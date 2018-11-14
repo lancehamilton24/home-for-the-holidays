891fcde32795269a6170a0b040e3351f3fceb1cb
Lance Hamilton <lance.hamilton24@gmail.com>
2018-11-13 20:04:07 -0600

    added google login button

--- components/Auth/auth.js
+++ components/Auth/auth.js
@@ -1,11 +1,15 @@
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+
+import googleImage from './google.png';
 import './auth.scss';
 
 const loginButton = () => {
   const domString = `
-    <button id="google-auth" class="btn btn-secondary">login</button>
+    <button id="google-auth" class="btn btn-secondary">
+      <img src= "${googleImage}"/>
+    </button>
   `;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
