27eb9a123250359e01455e1e1082ffb380e23777
Lance Hamilton <lance.hamilton24@gmail.com>
2018-11-19 20:12:36 -0600

    added ability to select a single friend from the drop down

--- components/FriendsPage/friendsPage.js
+++ components/FriendsPage/friendsPage.js
@@ -3,6 +3,33 @@ import $ from 'jquery';
 import apiKeys from '../../db/apiKeys.json';
 import authHelpers from '../../src/helpers/authHelpers';
 
+const printSingleFriend = (friend) => {
+  const friendString = `
+  <div>
+    <h1>${friend.name}</h1>
+    <h3>${friend.relationship}</h3>
+    <p>${friend.address}</p>
+    <p>${friend.email}</p>
+    <p>${friend.phoneNumber}</p>
+    <button class="btn btn-danger delete-btn">X</button>
+  </div>
+  `;
+  $('#single-container').html(friendString);
+};
+
+const getSingleFriend = (e) => {
+  const friendId = e.target.dataset.dropdownId;
+  console.log(friendId);
+  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends/${friendId}.json`)
+    .then((result) => {
+      const singleFriend = result.data;
+      singleFriend.id = friendId;
+      printSingleFriend(singleFriend);
+    })
+    .catch((error) => {
+      console.error('error ingetting one friend', error);
+    });
+};
 
 const buildDropdown = (friendsArray) => {
   let dropdown = `<div class="dropdown">
@@ -11,7 +38,7 @@ const buildDropdown = (friendsArray) => {
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
   friendsArray.forEach((friend) => {
-    dropdown += `<a class="dropdown-item" href="#">${friend.name}</div>`;
+    dropdown += `<div class="dropdown-item" data-dropdown-id=${friend.id}>${friend.name}</div>`;
   });
   dropdown += '<div></div>';
   $('#dropdown-container').html(dropdown);
@@ -36,5 +63,13 @@ const friendsPage = () => {
     });
 };
 
+const bindEvents = () => {
+  $('body').on('click', '.dropdown-item', getSingleFriend);
+};
 
-export default friendsPage;
+const initializeFriendsPage = () => {
+  friendsPage();
+  bindEvents();
+};
+
+export default initializeFriendsPage;
