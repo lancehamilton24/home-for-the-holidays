6c9d7af09af7c82863f5fa02e502c988398e6a25
Lance Hamilton <lance.hamilton24@gmail.com>
2018-11-26 19:11:07 -0600

    added holiday data

--- components/FriendsPage/friendsPage.js
+++ components/FriendsPage/friendsPage.js
@@ -1,37 +1,57 @@
 import $ from 'jquery';
 import authHelpers from '../../src/helpers/authHelpers';
 import friendsData from '../../src/helpers/data/friendsData';
+import holidayFriendsData from '../../src/helpers/data/holidayFriendsData';
+import holidaysData from '../../src/helpers/data/holidaysData';
 
-const printSingleFriend = (friend) => {
+const holidayStringBuilder = (holidays) => {
+  let holidayString = '<h3>Holidays:</h3>';
+  holidays.forEach((holiday) => {
+    holidayString += `<h5>${holiday.name}</h5>`;
+  });
+  return holidayString;
+};
+
+const printSingleFriend = (friend, holidays) => {
   const friendString = `
-  <div>
-    <h1>${friend.name}</h1>
-    <h3>${friend.relationship}</h3>
-    <p>${friend.address}</p>
-    <p>${friend.email}</p>
-    <p>${friend.phoneNumber}</p>
-    <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>X</button>
-    <button class="btn btn-info edit-btn" data-edit-id=${friend.id}>Edit</button>
-  </div>
+    <div>
+      <h1>${friend.name}</h1>
+      <h3>${friend.relationship}</h3>
+      <p>${friend.address}</p>
+      <p>${friend.email}</p>
+      <p>${friend.phoneNumber}</p>
+      <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>X</button>
+      <button class="btn btn-info edit-btn" data-edit-id=${friend.id}>Edit</button>
+      <div class="holiday-container">${holidayStringBuilder(holidays)}</div>
+    </div>
   `;
   $('#single-container').html(friendString);
 };
 
 const getSingleFriend = (e) => {
+  // firebase id
   const friendId = e.target.dataset.dropdownId;
+  const uid = authHelpers.getCurrentUid();
   friendsData.getSingleFriend(friendId)
     .then((singleFriend) => {
-      printSingleFriend(singleFriend);
+      holidayFriendsData.getHolidayIdsForFriend(friendId).then((holidayIds) => {
+        holidaysData.getHolidaysByArrayofIds(uid, holidayIds).then((holidays) => {
+          printSingleFriend(singleFriend, holidays);
+        });
+      });
+      // const holidayIds = ['holiday1'];
+      const holidays = ['a', 'b', 'c'];
+      printSingleFriend(singleFriend, holidays);
     })
     .catch((error) => {
-      console.error('error ingetting one friend', error);
+      console.error('error in getting one friend', error);
     });
 };
 
 const buildDropdown = (friendsArray) => {
   let dropdown = `<div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
+    Pick a Friend
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
   if (friendsArray.length) {
@@ -41,10 +61,10 @@ const buildDropdown = (friendsArray) => {
   } else {
     dropdown += '<div class="dropdown-item">You have no friends.</div>';
   }
-  dropdown += '<div></div>';
+
+  dropdown += '</div></div>';
   $('#dropdown-container').html(dropdown);
 };
-
 
 const friendsPage = () => {
   const uid = authHelpers.getCurrentUid();
@@ -58,6 +78,7 @@ const friendsPage = () => {
 };
 
 const deleteFriend = (e) => {
+  // firebase id
   const idToDelete = e.target.dataset.deleteId;
   friendsData.deleteFriend(idToDelete)
     .then(() => {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app';
 import 'bootstrap';
 import $ from 'jquery';
-import showAddForm from '../components/AddEditFriends/addEditFriends';
 
 import apiKeys from '../db/apiKeys.json';
 
 import createNavbar from '../components/Navbar/navbar';
 import loginButton from '../components/Auth/auth';
-
-import './index.scss';
 import authHelpers from './helpers/authHelpers';
 import friendsPage from '../components/FriendsPage/friendsPage';
+import showAddForm from '../components/AddEditFriends/addEditFriends';
+
+import './index.scss';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
