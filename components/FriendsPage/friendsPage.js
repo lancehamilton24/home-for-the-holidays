d310e5f0ed43590754e8ab66ed388a12c79e0961
Lance Hamilton <lance.hamilton24@gmail.com>
2018-11-26 20:51:52 -0600

    added avoid checkbox

--- components/FriendsPage/friendsPage.js
+++ components/FriendsPage/friendsPage.js
@@ -20,12 +20,17 @@ const printSingleFriend = (friend, holidays) => {
       <p>${friend.address}</p>
       <p>${friend.email}</p>
       <p>${friend.phoneNumber}</p>
-      <button class="btn btn-danger delete-btn" data-delete-id=${friend.id}>X</button>
-      <button class="btn btn-info edit-btn" data-edit-id=${friend.id}>Edit</button>
-      <div class="holiday-container">${holidayStringBuilder(holidays)}</div>
+      <div class="form-check form-check-inline">
+        <label class="form-check-label" for="inlineCheckbox1">Am I avoiding them?</label>
+        <input class="form-check-input is-avoiding-checkbox" type="checkbox" id="${friend.id}">
       </div>
+      <div class="holiday-container">${holidayStringBuilder(holidays)}</div>
+    </div>
   `;
   $('#single-container').html(friendString);
+  if (friend.isAvoiding) {
+    $('.is-avoiding-checkbox').attr('checked', true);
+  }
 };
 
 const getSingleFriend = (e) => {
@@ -86,9 +91,24 @@ const deleteFriend = (e) => {
     });
 };
 
+const updateIsAvoiding = (e) => {
+  const friendId = e.target.id;
+  const isAvoiding = e.target.checked;
+  friendsData.updatedIsAvoiding(friendId, isAvoiding)
+    .then(() => {
+
+    })
+    .catch((err) => {
+      console.err('error in updating flag', err);
+    });
+  console.log('you clicked checkbox');
+};
+
+
 const bindEvents = () => {
   $('body').on('click', '.get-single', getSingleFriend);
   $('body').on('click', '.delete-btn', deleteFriend);
+  $('body').on('change', '.is-avoiding-checkbox', updateIsAvoiding);
 };
 
 const initializeFriendsPage = () => {
